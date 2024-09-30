d15f5426c95d8036588a2c0cdc94c0a13cc64faa
Monika Mak <monika1986mak@gmail.com>
2024-09-30 15:37:31 +0000

    procfile update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn codestar.wsgi
+web: gunicorn ai_project.wsgi
--- ai_project/settings.py
+++ ai_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b)j-q%cmq^)@%&g66b_#p$bn98g_g=f*j-%0stjs#ilw1$yvc3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-monikamak-aipp4-nk7q3unjz4g.ws.codeinstitute-ide.net', '.herokuapp.com']
 
