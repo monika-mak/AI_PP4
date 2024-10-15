263a7b156865b0e0577a83d5303171b099add9bd
Monika Mak <monika1986mak@gmail.com>
2024-10-15 21:30:51 +0000

    views adjust , deployment settings

--- ai_blog/views.py
+++ ai_blog/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to AI Heroes!")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
