6e53cdb91f518f2be5f2ef5c36a54e84355b20e5
Monika Mak <monika1986mak@gmail.com>
2024-10-15 21:15:38 +0000

    add about app

--- ai_project/urls.py
+++ ai_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from ai_blog import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('ai', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
